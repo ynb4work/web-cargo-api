c2b949f84c01c2e2c4540e670a7e2f59d214f807
Ynb925 <ynb4work@gmail.com>
2024-04-15 15:06:09 +0300

    Initial commit

---
+++ web-cargo-api/src/main/java/com/webcargoapi/WebCargoApiApplication.java
@@ -0,0 +1,12 @@
+package com.webcargoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCargoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCargoApiApplication.class, args);
+	}
+}
---
+++ web-cargo-api/src/test/java/com/webcargoapi/WebCargoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webcargoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCargoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
